028834e8d4e73df0c83b5128a09476d489abfa44
Milan Malivuk <mmalivuk@gmail.com>
2012-01-27 23:54:33 -0500

    test outside model reference

--- app/mailers/seller_mailer.rb
+++ app/mailers/seller_mailer.rb
@@ -1,5 +1,6 @@
 class SellerMailer < ActionMailer::Base
   def registration_confirmation(seller)
+    @user = User.random
     mail(:to => seller.email, :subject => "Success! Keep an eye out for proposals.", :from => "admin@housr.ca")
   end
 end

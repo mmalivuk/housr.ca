4e95379feaf0ef3d38df9ed483a03b1c2d90a3ca
Milan Malivuk <mmalivuk@gmail.com>
2012-01-30 15:02:55 -0500

    tweak email variables

--- app/mailers/seller_mailer.rb
+++ app/mailers/seller_mailer.rb
@@ -1,6 +1,7 @@
 class SellerMailer < ActionMailer::Base
   def registration_confirmation(seller)
     users = User.random(5)
+    @seller = seller
     
     users.each do |user|
       mail(:to => user.email, :subject => "Here's your lead.", :from => "admin@housr.ca")

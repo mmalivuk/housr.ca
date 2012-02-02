150f624035758305909a2869cf6c8389de5039fb
Milan Malivuk <mmalivuk@gmail.com>
2012-02-02 14:50:35 -0500

    corrected distribution algorithm

--- app/mailers/seller_mailer.rb
+++ app/mailers/seller_mailer.rb
@@ -1,5 +1,5 @@
 class SellerMailer < ActionMailer::Base
-  def distribute(seller, user)
+  def distribute(seller)
     @seller = seller
     mail(:to => user.email, :subject => "You've received a new lead from housr.ca !", :from => "admin@housr.ca")
   end

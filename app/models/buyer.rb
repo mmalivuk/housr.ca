dfe10f034dd89468dec539fa01230e3bb839f39d
Milan Malivuk <mmalivuk@gmail.com>
2012-02-07 19:02:06 -0500

    buyer mailer variables fixed

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,5 +1,5 @@
 class Buyer < ActiveRecord::Base
-  validates :min_bedrooms, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :min_bathrooms, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :bedrooms, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :bathrooms, :numericality => { :greater_than_or_equal_to => 0 }
   validates :street_number, :numericality => { :greater_than_or_equal_to => 0 }
 end

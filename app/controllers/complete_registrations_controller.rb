38a4eb7ff9e54a3289d868f7feb977f70754d715
Milan Malivuk <mmalivuk@gmail.com>
2012-02-24 21:34:07 -0500

    chargify connected, wicked awesome progress bro

--- app/controllers/complete_registrations_controller.rb
+++ app/controllers/complete_registrations_controller.rb
@@ -1,4 +1,4 @@
-def CompleteRegistrationsController < ApplicationController
+class CompleteRegistrationsController < ApplicationController
   def new
     subscription = Chargify::Subscription.find(params[:subscription].to_i) rescue nil
     User.init!(subscription) if subscription.present?
